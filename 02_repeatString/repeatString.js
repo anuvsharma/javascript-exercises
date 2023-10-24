ed148456746cfd5d8bff069f708188d8c940aeb4
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-10-24 00:50:04 -0700

    completed task 2
    completed the second task of this exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, number) {
+
+    if (number<0){
+        return "ERROR"
+    }
+    let string = "";
+
+    for( let i = 0; i<number; i++){
+        string += text;
+    }
+    return string;
 
 };
 
