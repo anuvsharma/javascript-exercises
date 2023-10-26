2f48a15d6a15704931eb9d7c7f16ba54651ce5b9
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-10-25 20:13:27 -0700

    completed task 7
    this code completes task seven of the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrneTemp) {
+
+  if(fahrneTemp == 32){
+    return 0;
+  }
+
+  return ( Math.round( (fahrneTemp-32)*(5/9)*10 ) ) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsTemp) {
+
+  if(celsTemp == 0){
+    return 32;
+  }
+
+  return Math.round(((celsTemp*(9/5)+32)*10)) / 10;
+
 };
 
 // Do not edit below this line
