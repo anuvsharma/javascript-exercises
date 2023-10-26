af723250cf47b1e6e132bb18ebc741060a9ae75a
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-10-25 19:08:40 -0700

    completes task 5
    this code completes task five of the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    if ( ((typeof start !== "number") || (typeof end !== "number")) || (start < 0 || end < 0)) {
+        return "ERROR";
+    }
+
+    if (start > end){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+
+    let returnSum = 0;
+    for (let i = start; i <= end; i++){
+        returnSum += i;
+    }
+
+    return returnSum;
 
 };
 
