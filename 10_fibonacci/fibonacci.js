c350e26dcf0fb19f56f1f32ffda27b7471a1fc83
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-11-02 22:56:02 -0700

    completed fibonacci exercise
    the following code passes all of the tests for this exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 1){
+        return "OOPS";
+    }
+
+    if (num == 1 || num == 2){
+        return 1;
+    }
+        let firstNumber = 1;
+        let secondNumber = 1;
+        for(i = 1; i<num-1; i++){
+            result = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = result;
+        }
+        return result;
 };
 
 // Do not edit below this line
