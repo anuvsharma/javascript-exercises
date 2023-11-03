7dc164801c27e9254d1e568ef275efea5c8a1017
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-11-02 22:33:27 -0700

    completed the palindrome exercise
    the task for completing the palindrome exercise is done by the following code. all tests pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reverseString = string.toLowerCase().replace(/[^a-z0-9]/g, "").split("").reverse().join("");
+    return reverseString == string.toLowerCase().replace(/[^a-z0-9]/g, "");
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
